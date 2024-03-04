f1895e795045cc2b427c081843479243b1446050
prakash126ml <mtxit@MTXs-MacBook-Pro-2.local>
2024-03-04 21:52:20 +0530

    added logger and custom exception

---
+++ demo.py
@@ -0,0 +1,11 @@
+from us_visa.logger import logging
+
+from us_visa.exception import USvisaException
+
+import sys
+
+try:
+    a = 1 / "10"
+except Exception as e:
+    raise USvisaException(e, sys) from e
+logging.info("Welcome to custom logging")
